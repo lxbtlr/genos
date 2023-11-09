06caa31489326dbc1d7c90539abcf02abf84fc2e
Alex Butler <lxbtlr@pm.me>
2023-11-09 09:22:53 -0500

    add nix stuff

---
+++ default.nix
@@ -0,0 +1,21 @@
+
+{ pkgs ? import <nixpkgs> {} }:
+
+with pkgs;
+
+
+
+let
+
+
+
+in mkShell {
+
+  packages = [
+    (pkgs.python311.withPackages (ps: [
+      ps.numpy
+      ps.scipy
+      ps.matplotlib
+    ]))
+  ];
+}
