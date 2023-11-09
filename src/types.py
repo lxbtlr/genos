6addfa3bc19c4ac1ecd70970eacb6b34058b6341
Alex Butler <lxbtlr@pm.me>
2023-11-09 16:45:07 -0500

    add canvas dataclass builtin functions

--- src/types.py
+++ src/types.py
@@ -36,8 +36,18 @@ class Solution():
 class Canvas():
     sequence: list[Solution]
         
+    def swap(self, ind_1, ind_2):
+        """
+        given two indicies swap the position of the two Solutions
+        """
+        self.sequence[ind_1], self.sequence[ind_2] = self.sequence[ind_2], self.sequence[ind_1] 
+        return None
+
     def image(self):
-        
+        """
+        Composite all polygons into an image
+        """
+        #TODO: this
         return
 
 #HACK: currently only using RGBA, move to using *ARGS and toggle how they are
