b63b2af0ecd4e83b2b30bad302e410b8b3aba628
Alex Butler <lxbtlr@pm.me>
2023-11-30 08:38:16 -0500

    update max time

--- runner_script.sh
+++ runner_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 
 #$ -P ct-scicomp        # Specify the SCC project name you want to use
-#$ -l h_rt=02:00:00      # Specify the hard time limit for the job, (120 is max, HH:MM:SS)
+#$ -l h_rt=12:00:00      # Specify the hard time limit for the job, (120 is max, HH:MM:SS)
 # Specify when an email is sent (default is none).
 #$ -m beas              # currently set to send email when job: starts, ends, aborts and is suspended
 #$ -N ProHC-ImgRecon    # Give job a name
