2a50d8ea707b99409f4f5f404746dd924d7426fe
Alex Butler <lxbtlr@pm.me>
2023-12-03 16:40:45 -0500

    add onto script

--- local_debug_model.py
+++ local_debug_model.py
@@ -2,14 +2,29 @@ from src.simulation import Simulation
 import src.log_trace
 import logging
 
+# import src.arg_parse
+import time
+
+
+# Create the simulation environment folder
+START_TIME = "-".join(time.ctime().split()[1:4]).replace(":", ".")
+simulation_data_folder = src.log_trace.mk_folder_path(
+    folder_name="playground", sub_fldr_name=str(START_TIME)
+)
+
+# NOTE: Create logger and set the log file in the simulation environment we just made
 logger = logging.getLogger(__name__)
-logger = src.log_trace.setup_logger(logger)
+logger = src.log_trace.setup_logger(
+    logger, name="/".join([simulation_data_folder, "simulation"])
+)
+
 
 if __name__ == "__main__":
     small_test_sim = Simulation(
-        b_image="img/windows.jpg",
+        folder_path=simulation_data_folder,
+        b_image="img/1.png",
         o_image="img/test_output.jpg",
-        m_poly=10,
+        m_poly=20,
         stag_lim=100,
         n_evals=10000,
     )
